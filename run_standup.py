d7721de91dc5103c55c84d47749383c04bcbe5fb
Michael Thompson <actionscripted@gmail.com>
2023-10-02 23:04:14 -0400

    feat: script args and docs

--- run_standup.py
+++ run_standup.py
@@ -1,5 +1,41 @@
+"""Script to initiate and run the standup reporting process."""
+
+import argparse
+import logging
+
 from standup.runner import Runner
 
-if __name__ == "__main__":
+
+def configure_logging(level: str) -> None:
+    """Configure the logging level and format for the application.
+
+    Args:
+        level (str): The logging level.
+    """
+    logging_level = getattr(logging, level.upper(), logging.ERROR)
+    logging.basicConfig(
+        level=logging_level, format="%(asctime)s [%(levelname)s]: %(message)s"
+    )
+
+
+def main(args: argparse.Namespace) -> None:
+    """Execute the Runner to display information from providers.
+
+    Args:
+        args (argparse.Namespace): Command-line arguments.
+    """
+    configure_logging(args.log_level)
     runner = Runner()
     runner.run()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run the standup! Grab your coffee.")
+    parser.add_argument(
+        "--log-level",
+        choices=["critical", "error", "warning", "info", "debug"],
+        default="error",
+        help="Set the logging level.",
+    )
+    args = parser.parse_args()
+    main(args)
